82b46c9cc20789f8ef7b58d9cf2bd5d5c2aa79a1
nilbahaaeddine <nilbahaaeddine@gmail.com>
2020-05-15 00:15:44 +0200

    GROOOOS CHANGEMENT

---
+++ src/mappingratings.py
@@ -0,0 +1,122 @@
+from enum import Enum
+
+class NormalizedRatings(Enum):
+    FALSE = 1
+    MIXTURE = 2
+    TRUE = 3
+    OTHER = -1
+
+_normalization_dictionary = {  # type: Dict[str, Dict[str,NormalizedRatings]]
+    "politifact": {  # type: Dict[str,NormalizedRatings]
+        'incorrect': NormalizedRatings.FALSE,
+        'pants-fire': NormalizedRatings.FALSE,
+        'pants on fire': NormalizedRatings.FALSE,
+        'pants on fire!': NormalizedRatings.FALSE,
+        'false': NormalizedRatings.FALSE,
+        'mostly correct': NormalizedRatings.MIXTURE,
+        'mostly false': NormalizedRatings.MIXTURE,
+        'barely true': NormalizedRatings.MIXTURE,
+        'half true': NormalizedRatings.MIXTURE,
+        'half-true': NormalizedRatings.MIXTURE,
+        'mostly true': NormalizedRatings.MIXTURE,
+        'true': NormalizedRatings.TRUE,
+        'correct': NormalizedRatings.TRUE
+    },
+    "snopes": {  # type: Dict[str,NormalizedRatings]
+        'false': NormalizedRatings.FALSE,
+        'legend': NormalizedRatings.FALSE,
+        'mixture': NormalizedRatings.MIXTURE,
+        'mixture:': NormalizedRatings.MIXTURE,
+        'true': NormalizedRatings.TRUE,
+        'mostly false': NormalizedRatings.MIXTURE,
+        'mostly true': NormalizedRatings.MIXTURE,
+        'partly true': NormalizedRatings.MIXTURE,
+        'MIXTURE OF TRUE AND FALSE INFORMATION': NormalizedRatings.MIXTURE,
+        'MIXTURE OF TRUE AND FALSE INFORMATION:': NormalizedRatings.MIXTURE,
+        'MIXTURE OF ACCURATE AND  INACCURATE INFORMATION': NormalizedRatings.MIXTURE
+    },
+    "africacheck": {  # type: Dict[str,NormalizedRatings]
+        'incorrect': NormalizedRatings.FALSE,
+        'mostly-correct': NormalizedRatings.MIXTURE,
+        'correct': NormalizedRatings.TRUE
+    },
+    "factscan": {  # type: Dict[str,NormalizedRatings]
+        'false': NormalizedRatings.FALSE,
+        'true': NormalizedRatings.TRUE,
+        'Misleading': NormalizedRatings.OTHER
+    },
+    "truthorfiction": {  # type: Dict[str,NormalizedRatings]
+        'fiction': NormalizedRatings.FALSE,
+        'truth': NormalizedRatings.TRUE,
+        'truth & fiction': NormalizedRatings.MIXTURE,
+        'mostly fiction': NormalizedRatings.MIXTURE,
+        'truth & misleading': NormalizedRatings.MIXTURE,
+        'mostly truth': NormalizedRatings.MIXTURE
+    },
+    "checkyourfact": {  # type: Dict[str,NormalizedRatings]
+        'False': NormalizedRatings.FALSE,
+        'True': NormalizedRatings.TRUE,
+        'Mostly True': NormalizedRatings.MIXTURE,
+        'true/false': NormalizedRatings.MIXTURE,
+        'truth & misleading': NormalizedRatings.MIXTURE,
+        'mostly truth': NormalizedRatings.MIXTURE,
+        'misleading': NormalizedRatings.MIXTURE
+    },
+    "factcheck_aap": {
+        "True": NormalizedRatings.TRUE,
+        "False": NormalizedRatings.FALSE,
+        "Mostly True": NormalizedRatings.MIXTURE,
+        "Mostly False": NormalizedRatings.MIXTURE,
+        "Somewhat True": NormalizedRatings.MIXTURE,
+        "Somewhat False": NormalizedRatings.MIXTURE
+    },
+    "factuel_afp_fr": {
+        'Faux': NormalizedRatings.FALSE,
+        'Totalement faux': NormalizedRatings.FALSE,
+        'Démenti': NormalizedRatings.FALSE,
+        "C'est une oeuvre de fiction": NormalizedRatings.FALSE,
+        'Vrai': NormalizedRatings.TRUE,
+        'Totalement Vrai': NormalizedRatings.TRUE,
+        'Plutôt vrai': NormalizedRatings.MIXTURE,
+        'Trompeur': NormalizedRatings.MIXTURE,
+        'trompeur': NormalizedRatings.MIXTURE,
+        'Plutôt faux': NormalizedRatings.MIXTURE,
+        'Presque': NormalizedRatings.MIXTURE,
+        'Mélangé': NormalizedRatings.MIXTURE,
+        'Mélange': NormalizedRatings.MIXTURE,
+        'Inexact': NormalizedRatings.MIXTURE,
+        'Incertain': NormalizedRatings.MIXTURE,
+        'Imprécis': NormalizedRatings.MIXTURE,
+        'Exagéré': NormalizedRatings.MIXTURE,
+        'Douteux': NormalizedRatings.MIXTURE,
+    },
+    "factcheck_afp": {
+        'False': NormalizedRatings.FALSE,
+        'Fake': NormalizedRatings.FALSE,
+        'Mixed': NormalizedRatings.MIXTURE,
+        'Hoax': NormalizedRatings.FALSE,
+        'Falso': NormalizedRatings.FALSE,
+        'APRIL FOOL': NormalizedRatings.FALSE
+    },
+    "fullfact": {
+        'Correct': NormalizedRatings.TRUE,
+        'Incorrect': NormalizedRatings.FALSE,
+        'Not quite': NormalizedRatings.MIXTURE
+    }
+}
+
+def _standardize_name(original_name: str):
+    return original_name.strip().lower().replace("!", "").replace(":", "").replace("-", " ")
+
+def normalize(source_name, original_name) -> NormalizedRatings:
+    '''
+    Generate a normalized rating from the original ratings on each respective site
+    :param original_name:
+    :return normalized_rating: NormalizedRating
+    '''
+    try:
+        source = _normalization_dictionary[source_name]
+        normalized_value = source[_standardize_name(original_name)]
+    except KeyError:
+        normalized_value = NormalizedRatings.OTHER
+    return normalized_value
